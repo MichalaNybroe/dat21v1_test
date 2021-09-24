9e555cdcc9336d07b36eca8b4db718be038cabeb
Michala Nybroe <michalanim@gmail.com>
2021-09-24 15:45:30 +0200

    Add Menu class

---
+++ src/Menu.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<Item> menuItems;
+
+    public Menu(ArrayList<Item> menuItems) {
+        this.menuItems = menuItems;
+    }
+}
